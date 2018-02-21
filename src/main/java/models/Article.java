bad83112df17b0728cd75071dcfbfab3746d0e36
Soliman <ahmed.soliman9595@gmail.com>
2018-02-21 20:01:40 +0200

    add article model

--- src/main/java/models/Article.java
+++ src/main/java/models/Article.java
@@ -2,4 +2,53 @@ package models;
 
 public class Article {
 
+    private int authorId;
+    private int postId;
+    private String title;
+    private String authorFirstName;
+    private String authorLastName;
+    private String miniText;
+    private int peopleTalking;
+
+
+    public Article(int authorId, int postId, String title, String authorFirstName, String authorLastName, String miniText, int peopleTalking){
+        this.authorId = authorId;
+        this.postId = postId;
+        this.authorFirstName = authorFirstName;
+        this.authorLastName = authorLastName;
+        this.miniText = miniText;
+        this.peopleTalking = peopleTalking;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+
+    public String getMiniText() {
+        return miniText;
+    }
+
+    public int getPeopleTalking() {
+        return peopleTalking;
+    }
+
+
+
+
 }
