31dee62758e050039b9f21f25d7c2c232b64d4cc
Soliman <ahmed.soliman9595@gmail.com>
2018-02-22 20:31:19 +0200

    implement getRecommendedJobListing method body (intersection of skills)

--- src/main/java/database/ArangoHandler.java
+++ src/main/java/database/ArangoHandler.java
@@ -1,8 +1,12 @@
 package database;
 
 import com.arangodb.ArangoCollection;
+import com.arangodb.ArangoCursor;
 import com.arangodb.ArangoDB;
 import com.arangodb.ArangoDatabase;
+import com.arangodb.entity.BaseDocument;
+import com.arangodb.util.MapBuilder;
+import com.arangodb.velocypack.VPackSlice;
 import models.Article;
 import models.JobListing;
 import models.User;
@@ -10,6 +14,8 @@ import utils.ConfigReader;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
 
 public class ArangoHandler implements DatabaseHandler {
     private ConfigReader config;
@@ -44,8 +50,25 @@ public class ArangoHandler implements DatabaseHandler {
      * @param userId : the user seeking job listing recommendations
      * @return list of recommended job listings
      */
-    public ArrayList<JobListing> getRecommendedJobListing(int userId) {
-        return null;
+    public ArrayList<JobListing> getRecommendedJobListing(String userId) {
+        String jobCollectionName = config.getConfig("collection.job.name");
+        String userCollectionName = config.getConfig("collection.user.name");
+        BaseDocument document = dbInstance.collection(userCollectionName).getDocument(userId, BaseDocument.class);
+        String [] userSkills = (String []) document.getAttribute("skill");
+
+        //query for getting jobListing if there is a match between this jobListing and requesting user's skills
+        String query = "FOR job IN %s FILTER "
+                + "@skills ANY IN job.requiredSkills "
+                + "RETURN job";
+        //bind variables
+        Map<String, Object> bindVars = new MapBuilder().put("skills", userSkills).get();
+
+        //execute query
+        ArangoCursor<JobListing> cursor = dbInstance.query(query, bindVars,null, JobListing.class);
+
+        final ArrayList<JobListing> returnedResults = new ArrayList<>();
+        cursor.forEachRemaining(returnedResults::add);
+        return returnedResults;
     }
 
     /**
