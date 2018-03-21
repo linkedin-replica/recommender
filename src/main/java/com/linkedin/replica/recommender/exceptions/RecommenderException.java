4be9c0fc3bc5346f24c544429921138f0f294080
Omar Radwan <omarradwan213@gmail.com>
2018-03-21 14:07:05 +0200

    added recommender exception

---
+++ src/main/java/com/linkedin/replica/recommender/exceptions/RecommenderException.java
@@ -0,0 +1,8 @@
+package com.linkedin.replica.recommender.exceptions;
+
+public class RecommenderException extends RuntimeException {
+
+    public RecommenderException(String message) {
+        super(message);
+    }
+}
